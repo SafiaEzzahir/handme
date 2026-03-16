68f0a570d5f76ebd91eabdae535114849b4b67e6
SafiaEzzahir <safia.ezzahir@hotmail.com>
2026-03-16 19:40:18 +0000

    Store current stage in sessionStorage with help of bestie ghub copilot (ik ive done it b4 but idc)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
+import { useState } from 'react'
+
 import './App.css'
 
 function App() {
-  return (
-    <div className="App"></div>
-  )
+  const [currentStage, setCurrentStage] = useState("StartPage");
+
+  useEffect(() => {
+    const savedStage = sessionStorage.getItem("currentStage") || "StartPage";
+    setCurrentStage(savedStage);
+    sessionStorage.setItem("currentStage", savedStage);
+  }, []);
+
+  if (currentStage === "StartPage") {
+    return (
+      <div className="App">
+        <h1>Welcome to the Game!</h1>
+        <button onClick={() => setCurrentStage("AuthTime")}>Start Game</button>
+      </div>
+    )
+  } else if (currentStage === "AuthTime") {
+  } else if (currentStage === "Game") {
+  }
 }
 
 export default App
